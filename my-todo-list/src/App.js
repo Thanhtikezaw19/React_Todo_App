c50414cc610b84ddecc33106ab080e5bce45f473
Jin Xin <101238424+than-htikezaw@users.noreply.github.com>
2022-11-09 21:52:50 +0630

    [todo complete]

--- my-todo-list/src/App.js
+++ my-todo-list/src/App.js
@@ -4,21 +4,37 @@ import './App.css';
 import Header from './components/Header';
 import data from "./components/data.json";
 import ToDoList from "./components/ToDoList";
+import ToDoForm from './components/ToDoForm';
 
 function App() {
+
   const [ toDoList, setToDoList ] = useState(data);
 
   const handleToggle = (id) => {
     let mapped = toDoList.map(task => {
-      return task.id == id ? { ...task, complete: !task.complete } : { ...task }
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
     });
-    this.setState({ toDoList: mapped });
+    setToDoList(mapped);
   }
-  
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+
+  const addTask = (userInput ) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+
   return (
     <div className="App">
       <Header />
-      <ToDoList toDoList={toDoList}/>
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+      <ToDoForm addTask={addTask}/>
     </div>
   );
 }
--- my-todo-list/src/components/ToDo.js
+++ my-todo-list/src/components/ToDo.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const ToDo = ({todo}) => {
-   return (
-       <div className={todo.complete ? "strike" : ""}>
-           {todo.task}
-       </div>
-   );
+const ToDo = ({todo, handleToggle}) => {
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        handleToggle(e.currentTarget.id)
+    }
+
+    return (
+        <div id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} onClick={handleClick} className={todo.complete ? "todo strike" : "todo"}>
+            {todo.task}
+        </div>
+    );
 };
 
 export default ToDo;
